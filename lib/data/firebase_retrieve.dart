aaccaaa4968e1e3b31993b2d6336ee192a428072
abdou-ashraf <abdouashrat123@gmail.com>
2025-03-03 20:56:32 +0200

    add Firebase video retrieval methods; implement getMyVideos and getMyVideoData functions; update custom drawer to fetch videos on tap

--- lib/data/firebase_retrieve.dart
+++ lib/data/firebase_retrieve.dart
@@ -53,4 +53,55 @@ class FirebaseRetrieve {
     }
     return ['Null', 'Null'];
   }
+
+  Future getMyVideos() async {
+    try {
+      List<Map<String, dynamic>> videosList = [];
+      final String uid = FirebaseAuth.instance.currentUser!.uid;
+      var userData = await FirebaseFirestore.instance
+          .collection('students')
+          .doc(uid)
+          .get();
+      String graduate = userData.data()!['userMainData']['graduate'];
+      // print(graduate);
+      if (graduate == '3') {
+        graduate = '3rd_secondary';
+      } else if (graduate == '2') {
+        graduate = '2nd_secondary';
+      } else {
+        graduate = '1st_secondary';
+      }
+      videosList.add({'graduate': graduate});
+      var videosData = await FirebaseFirestore.instance
+          .collection('students')
+          .doc(uid)
+          .collection('user_vid')
+          .get();
+
+      var videos = videosData.docs;
+      for (var video in videos) {
+        videosList.add(video.data());
+      }
+
+      //  print(videosList);
+    } catch (e) {
+      print(e.toString() + ' =======================');
+    }
+    return ['Null', 'Null'];
+  }
+
+  Future getMyVideoData(String videoId, String graduate, String section) async {
+    try {
+      var snapshot = await FirebaseFirestore.instance
+          .collection(graduate)
+          //TODO : change this
+          .doc('section')
+          .collection(section)
+          .doc(videoId)
+          .get();
+      return snapshot.data();
+    } catch (e) {
+      print(e.toString() + ' =======================');
+    }
+  }
 }
