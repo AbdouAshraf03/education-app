8faac5f3a5a3f19837b52a0ba3f9e8bbd4cc73f7
abdou <abdouashrat123@gmail.com>
2025-03-05 00:19:17 +0200

    implement video retrieval in VideoPage; add FutureBuilder for async data loading and handle loading/error states

--- lib/views/main_pages/video_page.dart
+++ lib/views/main_pages/video_page.dart
@@ -1,17 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:mr_samy_elmalah/data/firebase_retrieve.dart';
+import 'package:mr_samy_elmalah/widgets/custom_drawer.dart';
+import 'package:mr_samy_elmalah/widgets/small_widgets.dart';
 import 'package:mr_samy_elmalah/widgets/videos_card.dart';
 
 class VideoPage extends StatelessWidget {
   const VideoPage({super.key});
+  Future<List> _getVideos() async {
+    List videosData;
+    await FirebaseRetrieve().getMyVideos().then((value) async {
+      videosData = await FirebaseRetrieve().getMyVideosFromList(value);
+      //  print("Videos: $videosData");
+      return videosData;
+    });
+    return ['Null'];
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(
-        top: 1.0,
-      ),
-      child: ListView.builder(
-          itemCount: 3, itemBuilder: (context, index) => MyVideosCard()),
+    return FutureBuilder(
+      future: _getVideos(),
+      builder: (context, snapshot) {
+        print(
+            "FutureBuilder called with snapshot: ${snapshot.connectionState}"); // Debugging statement
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            body: const Center(
+              child: LottieLoader(),
+            ),
+          );
+        }
+        if (snapshot.hasData) {
+          return Padding(
+            padding: const EdgeInsets.only(
+              top: 1.0,
+            ),
+            child: ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) => MyVideosCard()),
+          );
+        }
+        print(
+            "Snapshot has no data or error: ${snapshot.error}"); // Debugging statement
+        return const Center(
+          child: Text('Error', style: TextStyle(color: Colors.red)),
+        );
+      },
     );
   }
 }
