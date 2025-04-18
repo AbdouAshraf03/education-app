edacc2fda79b6b435b9d61989d9e9a3f21cbf6a2
abdou-ashraf <abdouashrat123@gmail.com>
2025-04-18 21:08:11 +0200

    feat: add image asset references for first, second, and third year

---
+++ lib/core/app_assets.dart
@@ -0,0 +1,10 @@
+abstract class ImageAppAssets {
+  static const String logoNoPg = 'assets/images/3math_logo-removebg.png';
+  static const String logoWhite = 'assets/images/3math logo.png';
+  static const String facebook48 = 'assets/images/icons8-facebook-48.png';
+  static const String facebook96 = 'assets/images/icons8-google-96.png';
+  static const String google48 = 'assets/images/icons8-google-48.png';
+  static const String firstYear = 'assets/images/1st.png';
+  static const String secondYear = 'assets/images/2nd.png';
+  static const String thirdYear = 'assets/images/3rd.png';
+}
